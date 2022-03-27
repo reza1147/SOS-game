a1942d1afbd573eed7cc715a19f33e68d9fc4f26
reza147 <reza.mohammadzadeh.1997@gmail.com>
2022-03-28 00:13:40 +0430

    Complete gameBord

--- src/Game.java
+++ src/Game.java
@@ -2,19 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class Game extends JFrame {
     GameBoard board;
     Rectangle[][] rectangles;
     private int cellSize;
     private int margin;
-    private Rectangle current,oldCurrent;
-
+    private int currentX = -1, currentY = -1, oldCurrentX = -1, oldCurrentY = -1;
+    private Font defaultFont = new Font("tahoma", Font.BOLD, 60);
     public Game(GameBoard board) {
         this.board = board;
         cellSize = 70;
         margin = 50;
-        setSize(800, 800);
+        setSize(board.getBoardWidth() * cellSize + 2 * margin,
+                board.getBoardHeight() * cellSize + 2 * margin);
         setLocationRelativeTo(null);
         setTitle("game");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -26,20 +28,53 @@ public class Game extends JFrame {
         addMouseMotionListener(new MouseAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
-                Rectangle temp=null;
+                int tempX = -1;
+                int tempY = -1;
                 if (e.getX() >= margin &&
                         e.getY() >= margin &&
-                        e.getX() <= cellSize * board.getBoardWidth()+margin-4 &&
-                        e.getY() <= cellSize * board.getBoardHeight()+margin-4)
-                    temp = rectangles[(e.getY() - margin) / cellSize][(e.getX() - margin) / cellSize];
-                if(temp!=current) {
-                    oldCurrent=current;
-                    current=temp;
+                        e.getX() <= cellSize * board.getBoardWidth() + margin - 4 &&
+                        e.getY() <= cellSize * board.getBoardHeight() + margin - 4) {
+                    tempX = (e.getY() - margin) / cellSize;
+                    tempY = (e.getX() - margin) / cellSize;
+                    if (board.getState(tempX, tempY) != null) {
+                        tempX = tempY = -1;
+                    }
+                }
+                if (tempX != currentX || tempY != currentY) {
+                    oldCurrentX = currentX;
+                    oldCurrentY = currentY;
+                    currentX = tempX;
+                    currentY = tempY;
                     repaint();
+                }
+            }
+
+        });
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (currentX != -1 && currentY != -1) {
+                    play();
                 }
             }
         });
 
+    }
+
+    private void play() {
+        String[] options = new String[]{"S", "O"};
+        setEnabled(false);
+        int answer = JOptionPane.showOptionDialog(this, "what you want?",
+                board.getTurn() ? "Blue Player" : "Red Player",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        if (answer != -1) {
+            lines.addAll(board.play(answer == 0, currentX, currentY));
+            wasPlayed = true;
+            repaint();
+        }
+        setEnabled(true);
+        requestFocus();
     }
 
     private void initTable() {
@@ -52,25 +87,53 @@ public class Game extends JFrame {
         }
     }
 
+    boolean wasPlayed = false;
+    boolean firstPaint = true;
+    ArrayList<int[]> lines=new ArrayList<>();
     @Override
     public void paint(Graphics g) {
-        g.setColor(Color.YELLOW);
-        if(current!=null)
-            g.fillRect(current.x+2, current.y+2,
-                    current.width-4, current.height-4);
-        g.setColor(Color.WHITE);
-        if(oldCurrent!=null)
-            g.fillRect(oldCurrent.x+2, oldCurrent.y+2,
-                    oldCurrent.width-4, oldCurrent.height-4);
-        g.setColor(Color.BLACK);
-        ((Graphics2D) g).setStroke(new BasicStroke(4));
-        for (int i = 0; i < board.getBoardHeight(); i++) {
-            for (int j = 0; j < board.getBoardWidth(); j++) {
-                g.drawRect(rectangles[i][j].x, rectangles[i][j].y,
-                        rectangles[i][j].width, rectangles[i][j].height);
+        if (firstPaint) {
+            g.setColor(Color.BLACK);
+            ((Graphics2D) g).setStroke(new BasicStroke(4));
+            for (int i = 0; i < board.getBoardHeight(); i++) {
+                for (int j = 0; j < board.getBoardWidth(); j++) {
+                    g.drawRect(rectangles[i][j].x, rectangles[i][j].y,
+                            rectangles[i][j].width, rectangles[i][j].height);
+                }
             }
+            firstPaint = false;
         }
-
+        if (wasPlayed) {
+            g.setColor(board.getTurn() ?
+                    new Color(255, 100, 100) :
+                    new Color(100, 100, 255));
+            g.fillRect(rectangles[currentX][currentY].x + 2, rectangles[currentX][currentY].y + 2,
+                    rectangles[currentX][currentY].width - 4, rectangles[currentX][currentY].height - 4);
+            g.setColor(board.getTurn() ? Color.BLACK : Color.WHITE);
+            g.setFont(defaultFont);
+            g.drawString(board.getState(currentX, currentY) ? "S" : "O",
+                    rectangles[currentX][currentY].x + 15, rectangles[currentX][currentY].y + 55);
+            g.setColor(board.getTurn() ? Color.RED : Color.blue);
+            ((Graphics2D) g).setStroke(new BasicStroke(4));
+            while(!lines.isEmpty()){
+                int[] coordinates=lines.remove(0);
+                g.drawLine(rectangles[coordinates[0]][coordinates[1]].x+cellSize/2,
+                        rectangles[coordinates[0]][coordinates[1]].y+cellSize/2,
+                        rectangles[coordinates[2]][coordinates[3]].x+cellSize/2,
+                        rectangles[coordinates[2]][coordinates[3]].y+cellSize/2);
+            }
+            currentX = currentY = oldCurrentX = oldCurrentY = -1;
+            wasPlayed = false;
+        } else {
+            g.setColor(Color.YELLOW);
+            if (currentX != -1)
+                g.fillRect(rectangles[currentX][currentY].x + 2, rectangles[currentX][currentY].y + 2,
+                        rectangles[currentX][currentY].width - 4, rectangles[currentX][currentY].height - 4);
+            g.setColor(Color.WHITE);
+            if (oldCurrentX != -1)
+                g.fillRect(rectangles[oldCurrentX][oldCurrentY].x + 2, rectangles[oldCurrentX][oldCurrentY].y + 2,
+                        rectangles[oldCurrentX][oldCurrentY].width - 4, rectangles[oldCurrentX][oldCurrentY].height - 4);
+        }
     }
 
     public static void main(String[] args) {
--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,18 +1,105 @@
+import java.util.ArrayList;
+
 public class GameBoard {
     // SR-SB-OR-OB-E -> int() - boolean Boolean
     private Boolean[][] board;// true = S   false = O  null = E
     private Boolean[][] owner;// true = B   false = R  null = E
     private boolean GameType; // true = PvE   false= PvP
-    public GameBoard(int w, int h , boolean type){
-        board=new Boolean[h][w];
-        owner=new Boolean[h][w];
-        GameType=type;
+    private boolean turn; // true = B   false = R
+    private int bPoint;
+    private int rPoint;
+    public GameBoard(int w, int h, boolean type) {
+        board = new Boolean[h][w];
+        owner = new Boolean[h][w];
+        GameType = type;
+        turn = true;
+        bPoint=0;
+        rPoint=0;
     }
 
-    public int getBoardWidth(){
+    public int getBPoint() {
+        return bPoint;
+    }
+
+    public int getRPoint() {
+        return rPoint;
+    }
+
+    public int getBoardWidth() {
         return board[0].length;
     }
-    public int getBoardHeight(){
+
+    public int getBoardHeight() {
         return board.length;
+    }
+
+    // true = B   false = R
+    public boolean getTurn() {
+        return turn;
+    }
+
+    //state-> true = S   false = O  null = E
+    public ArrayList<int[]> play(boolean state, int x, int y) {
+        ArrayList<int[]> lines= new ArrayList<>();
+        board[x][y] = state;
+        owner[x][y] = turn;
+        if (state)
+            lines.addAll(checkGetPointS(x, y));
+        else
+            lines.addAll(checkGetPointO(x, y));
+        if(turn)
+            bPoint+=lines.size();
+        else
+            rPoint+=lines.size();
+        turn = !turn;
+        return lines;
+    }
+
+    private ArrayList<int[]> checkGetPointO(int x, int y) {
+        ArrayList<int[]> lines= new ArrayList<>();
+        for (int i = -1; i <= 1; i++) {
+            for (int j = 0; j <= 1; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                if (i == -1 && j == 0)
+                    continue;
+                if (x + i >= getBoardHeight() || x + i < 0 || y + j >= getBoardWidth() || y + j < 0)
+                    continue;
+                if (x - i >= getBoardHeight() || x - i < 0 || y + j >= getBoardWidth() || y - j < 0)
+                    continue;
+                if (board[x + i][y + j] == null || board[x - i][y - j] == null)
+                    continue;
+                if (board[x + i][y + j] && board[x - i][y - j]) {
+                    lines.add(new int[]{x + i,y + j,x - i,y - j});
+                }
+            }
+        }
+        return lines;
+    }
+
+    private ArrayList<int[]> checkGetPointS(int x, int y) {
+        ArrayList<int[]> lines= new ArrayList<>();
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                if (x + i >= getBoardHeight() || x + i < 0 || y + j >= getBoardWidth() || y + j < 0)
+                    continue;
+                if (x + i + i >= getBoardHeight() || x + i + i < 0 || y + j + j >= getBoardWidth() || y + j + j < 0)
+                    continue;
+                if (board[x + i][y + j] == null || board[x + i + i][y + j + j] == null)
+                    continue;
+                if (!board[x + i][y + j] && board[x + i + i][y + j + j]) {
+                    lines.add(new int[]{x ,y ,x + i + i,y + j + j});
+
+                }
+            }
+        }
+        return lines;
+
+    }
+
+    public Boolean getState(int x, int y) {
+        return board[x][y];
     }
 }
